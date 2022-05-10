1d19be9b4c2911a1701c1515a4cab3f41fec8898
Brian Jenkins <bj@jenkinz.com>
2022-05-10 13:45:11 -0700

    Add DestinationCard vue component
    Image URL not working for some reason.

---
+++ src/components/DestinationCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+  destination: {
+    type: Object,
+    default() {
+      return {
+        city: 'Toronto',
+        averagePrice: 120,
+        propertyCount: 76,
+        imageUrl: './assets/toronto.png',
+        imageAlt: 'Toronto skyline',
+      };
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="flex items-center overflow-hidden rounded-lg bg-white shadow-lg">
+    <img
+      class="h-32 w-32 flex-shrink-0"
+      :src="destination.imageUrl"
+      :alt="destination.imageAlt"
+    />
+    <div class="px-6 py-4">
+      <h3 class="text-lg font-semibold text-gray-800">
+        {{ destination.city }}
+      </h3>
+      <p class="text-gray-600">
+        ${{ destination.averagePrice }} / night average
+      </p>
+      <div class="mt-4">
+        <a href="#" class="font-semibold text-indigo-500 hover:text-indigo-400">
+          Explore {{ destination.propertyCount }} properties
+        </a>
+      </div>
+    </div>
+  </div>
+</template>

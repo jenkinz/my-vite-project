f702ebcc89cf775541695666a1647e94c13e4ce5
Brian Jenkins <bj@jenkinz.com>
2022-05-13 18:19:07 -0700

    Add example config error handler to app

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.config.errorHandler = (err, instance, info) => {
+  // handle error...
+  // report error to tracking services...
+};
+
+app.mount('#app');
